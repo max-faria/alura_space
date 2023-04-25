ca57d1d5e858b2989e8da4827f12a61780f955e4
Max Faria <mxxfaria@gmail.com>
2023-04-24 22:40:44 -0300

    projeto alura space

---
+++ galeria/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from galeria.views import index, imagem
+
+urlpatterns = [
+    path('', index)
+]
+
+urlpatterns = [
+    path('', index),
+    path('imagem/', imagem)
+]
